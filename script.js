2af5b2ed8eaa5fb7ccd6eec238db5dfc8f8f231b
arzumammadov1 <mi7fqqg3l@code.edu.az>
2023-10-29 21:07:45 +0400

    js.task

---
+++ script.js
@@ -0,0 +1,68 @@
+// Task 1
+
+// const arr=[2,3,4,5,6,7,8]
+// let cem=0
+// let count=0
+// let edediorta=0
+// arr.forEach((x) => {
+//     cem=cem+x
+//     count=count+1
+//     edediorta=cem/count
+// })
+// console.log(`verilmis edelerin cemi ${cem}-dir`);
+// console.log(`verilmis eded sayi ${count}-dir`);
+// console.log(`EDEDI ORTA ${edediorta}-dir`);
+
+
+
+
+
+// Task 2
+
+
+// const arr = [2,4,3,5,6,7,3,9]
+// let cutcem=0
+// arr.forEach((x)=>{
+//     if(x%2===0){
+//         cutcem=cutcem+x
+//     }
+// })
+// console.log(cutcem);
+
+
+
+
+
+// Task 3
+
+// const arr = [2,4,3,5,6,7,3,1]
+// let tekcem=0
+// arr.forEach((x)=>{
+//     if(x%2!==0){
+//         tekcem=tekcem+x
+//     }
+// })
+// console.log(tekcem);
+
+
+
+
+// Task 4
+
+// const arr = [2,4,3,5,6,7,3,1]
+// let tekcem=0
+// let cutcem=0
+// arr.forEach((x)=>{
+//     if (x%2===0) {
+//         cutcem=cutcem+x
+        
+//     }
+//     if (x%2!==0) {
+//         tekcem=tekcem+x
+        
+//     }
+// })
+// console.log(`cutlerin cemi ${cutcem}-dir`);
+// console.log(`teklerin cemi ${tekcem}-dir`);
+// console.log(`teklerin cemi ile cutlerin ceminin hasili ${cutcem*tekcem}-dir`);
+
